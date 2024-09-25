c15f2798bd3da143b0900196f13b8cf5dad2b818
Manuel Gómez <manuel.gomez@gicla.com>
2024-09-24 21:11:40 -0300

    - Add styles for champion selection page

--- src/app/[id]/champions/team.tsx
+++ src/app/[id]/champions/team.tsx
@@ -1,15 +1,73 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const isBan: boolean = true;
 
 function Team() {
-    return (
-        <div className="flex flex-row gap-3">
-            <div className="banChamp"><FontAwesomeIcon className="iconBan" icon="ban" /></div>
-            <div className="banChamp"><FontAwesomeIcon className="iconBan" icon="ban" /></div>
-            <div className="banChamp"><FontAwesomeIcon className="iconBan" icon="ban" /></div>
-            <div className="banChamp"><FontAwesomeIcon className="iconBan" icon="ban" /></div>
-            <div className="banChamp"><FontAwesomeIcon className="iconBan" icon="ban" /></div>
+  return (
+    <>
+      <div className="flex flex-row gap-3 mb-10 mt-10 ml-4">
+        <div className="banChamp">
+          <FontAwesomeIcon className="iconBan" icon="ban" />
         </div>
-    );
+        <div className="banChamp">
+          <FontAwesomeIcon className="iconBan" icon="ban" />
+        </div>
+        <div className="banChamp">
+          <FontAwesomeIcon className="iconBan" icon="ban" />
+        </div>
+        <div className="banChamp">
+          <FontAwesomeIcon className="iconBan" icon="ban" />
+        </div>
+        <div className="banChamp">
+          <FontAwesomeIcon className="iconBan" icon="ban" />
+        </div>
+      </div>
+      <div>
+        <div className="flex flex-col gap-3">
+          {isBan ? (
+            <div className="selecChamp">
+                <div className="containerSelecChampSeparador up flex items-center">
+                    <div className="puntoSelecChampSeparador"></div>
+                    <hr className="lineaSelecChampSeparador" />
+                </div>
+                <div className="separadorAmarillo left">
+                </div>
+                <div className="blockChamp">
+                  <FontAwesomeIcon className="iconBlock" icon="ban" />
+                </div>
+                <div className="circuloCampeonSeleccionado">
+                </div>
+                <div className="flex flex-col gap-1 text-[9px] containerNickUsuario">
+                  <label>
+                    Bloqueo
+                  </label>
+                  <label>
+                    Nick name usuario
+                  </label>
+                </div>
+                <FontAwesomeIcon className="iconArrowSelec" icon="caret-left" />
+                <div className="containerContandorUsuario">
+                  22
+                </div>
+                <div className="containerSelecChampSeparador bottom flex items-center">
+                    <div className="puntoSelecChampSeparador"></div>
+                    <hr className="lineaSelecChampSeparador" />
+                </div>
+            </div>
+          ) : (
+            <div className="selecBan">
+
+            </div>
+          )}
+          <div className="selecChamp"></div>
+          <div className="selecChamp"></div>
+          <div className="selecChamp"></div>
+          <div className="selecChamp"></div>
+          <div className="selecChamp"></div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Team;
--- src/lib/fontawesome.js
+++ src/lib/fontawesome.js
@@ -1,7 +1,8 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core';
-import { faBan } from '@fortawesome/free-solid-svg-icons';
+import { faBan, faCaretLeft } from '@fortawesome/free-solid-svg-icons';
 import '@fortawesome/fontawesome-svg-core/styles.css'; // Importa los estilos
 
 config.autoAddCss = false; // Evita que Font Awesome añada automáticamente los estilos CSS
 
 library.add(faBan);
+library.add(faCaretLeft);
